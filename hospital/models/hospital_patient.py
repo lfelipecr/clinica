d171aa1e4882e9e1a3f48a2f6f6371419abe7544
Felipe Montero <felipemontero@Felipes-MacBook-Pro.local>
2024-11-14 09:25:27 -0600

    Se agrega el campo contacto

--- hospital/models/hospital_patient.py
+++ hospital/models/hospital_patient.py
@@ -7,11 +7,18 @@ class HospitalPatient(models.Model):
     _description = 'Patients'
 
     name = fields.Char(string='Nombre', required=True, tracking=True)
+    
     sequence = fields.Char(string='Sequence', default="New", readonly=True)
     age = fields.Integer(string='Edad', required=True, tracking=True)
     gender = fields.Selection(string='Genero', selection=[('male', 'Masculino'), ('female', 'Femenino')], default='male', tracking=True)
     email = fields.Char(string='Correo')
     company_id = fields.Many2one(comodel_name='res.company', default=lambda self: self.env.company)
+    contact_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Contacto',
+        ondelete='set null',
+        help='Selecciona el contacto asociado a este paciente.'
+    )
 
     @api.model_create_multi
     def create(self, vals_list):
