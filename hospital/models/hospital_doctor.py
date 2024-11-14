38ba5c43357dea3a63da6e8bb9c1bcfe15e799bd
Felipe Montero <felipemontero@Felipes-MacBook-Pro.local>
2024-11-13 18:06:18 -0600

    Corregir comillas

--- hospital/models/hospital_doctor.py
+++ hospital/models/hospital_doctor.py
@@ -6,7 +6,7 @@ class HospitalDoctor(models.Model):
     _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
     _description = 'Doctor'
 
-    name = fields.Char(string="Nombre, required=True, tracking=True)
+    name = fields.Char(string="Nombre", required=True, tracking=True)
     age = fields.Integer(string='Edad', required=True, tracking=True)
     gender = fields.Selection(string='Genero', selection=[('male', 'Masculino'), ('female', 'Femenino')], default='male')
     specialty = fields.Char(string='Specialidad', required=True, tracking=True)
